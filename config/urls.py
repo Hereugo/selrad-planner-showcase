1894eb4b54a68e3ef1085a536d0233878d26687a
Амир Нурмухамбетов <thereug6@gmail.com>
2024-03-19 15:49:31 +0100

    working version

--- config/urls.py
+++ config/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
-from plans.views import PlanListView, plan_create, index, plans_excel, plan_show_map, plan_delete, aside_buttons, get_client_address
+from plans.views import PlanListView, plan_create, index, plans_excel, plan_show_map, aside_buttons, get_client_address
 
 
 urlpatterns = [
