1fa0e955049b6b90abf065281805fde8e96b24df
nomomon <funisinfinity26@gmail.com>
2024-03-29 08:45:54 +0100

    Add basic Yandex Maps component to MapsTemplate

--- frontend/components/templates/maps/index.tsx
+++ frontend/components/templates/maps/index.tsx
@@ -1,9 +1,46 @@
-import { FC } from "react";
+"use client";
 
-interface MapsTemplateProps {}
+import { FC } from "react";
+import { Polygon, YMaps, Map } from '@pbe/react-yandex-maps';
+
+interface MapsTemplateProps { }
 
 const MapsTemplate: FC<MapsTemplateProps> = () => {
-  return <>Maps</>;
+    return (
+        <YMaps>
+            <Map
+                defaultState={{
+                    center: [55.73, 37.9],
+                    zoom: 10,
+                }}
+                className="w-full h-full rounded-lg overflow-clip"
+            >
+                <Polygon
+                    geometry={[
+                        [
+                            [55.75, 37.8],
+                            [55.8, 37.9],
+                            [55.75, 38.0],
+                            [55.7, 38.0],
+                            [55.7, 37.8],
+                        ],
+                        [
+                            [55.75, 37.82],
+                            [55.75, 37.98],
+                            [55.65, 37.9],
+                        ],
+                    ]}
+                    options={{
+                        fillColor: "#00FF00",
+                        strokeColor: "#0000FF",
+                        opacity: 0.5,
+                        strokeWidth: 5,
+                        strokeStyle: "shortdash",
+                    }}
+                />
+            </Map>
+        </YMaps>
+    )
 };
 
 export default MapsTemplate;
