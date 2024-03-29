b6b92023910b93008b1f2ec5adf1cbd2f29a9f59
nomomon <funisinfinity26@gmail.com>
2024-03-29 06:31:04 +0100

    Fix attributes in TengeReciept component

--- frontend/components/icons/tenge-reciept.tsx
+++ frontend/components/icons/tenge-reciept.tsx
@@ -2,29 +2,29 @@ import { cn } from '@/lib/utils';
 import { FC } from 'react';
 
 interface TengeRecieptProps {
-	className?: string;
+    className?: string;
 }
 
 export const TengeReciept: FC<TengeRecieptProps> = ({ ...props }) => {
-	return (
-		<svg
-			xmlns="http://www.w3.org/2000/svg"
-			width="24"
-			height="24"
-			viewBox="0 0 24 24"
-			fill="none"
-			stroke="currentColor"
-			stroke-width="2"
-			stroke-linecap="round"
-			stroke-linejoin="round"
-			className={cn('lucide lucide-receipt', props['className'])}
-		>
-			<path d="M4 2v20l2-1 2 1 2-1 2 1 2-1 2 1 2-1 2 1V2l-2 1-2-1-2 1-2-1-2 1-2-1-2 1Z" />
-			<path d="M12 17.5v-8" />
-			<path d="M8 9.5h8" />
-			<path d="M8 7h8" />
-		</svg>
-	);
+    return (
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            stroke="currentColor"
+            strokeWidth="2"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            className={cn('lucide lucide-receipt', props['className'])}
+        >
+            <path d="M4 2v20l2-1 2 1 2-1 2 1 2-1 2 1 2-1 2 1V2l-2 1-2-1-2 1-2-1-2 1-2-1-2 1Z" />
+            <path d="M12 17.5v-8" />
+            <path d="M8 9.5h8" />
+            <path d="M8 7h8" />
+        </svg>
+    );
 };
 
 export const TengeRecieptIcon = TengeReciept;
