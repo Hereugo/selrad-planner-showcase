58fe5debd9a49f9b642857cc358cec6a11796734
nomomon <funisinfinity26@gmail.com>
2024-03-29 16:36:10 +0100

    Refactor day picker component and update next.config and package.json

--- frontend/components/molecules/plan-dialog-edit/day-picker.tsx
+++ frontend/components/molecules/plan-dialog-edit/day-picker.tsx
@@ -9,60 +9,60 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Calendar } from "@/components/ui/calendar";
 import {
-    Popover,
-    PopoverContent,
-    PopoverTrigger,
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
 } from "@/components/ui/popover";
 
 interface DayPickerProps {
-    id?: string;
-    assignedDate: string;
-    setAssignedDate: (date: string) => void;
+  id?: string;
+  assignedDate: string;
+  setAssignedDate: (date: string) => void;
 }
 
 const DayPicker: FC<DayPickerProps> = ({
-    id,
-    assignedDate,
-    setAssignedDate,
+  id,
+  assignedDate,
+  setAssignedDate,
 }) => {
-    const [date, setDate] = useState<Date | undefined>(new Date(assignedDate));
+  const [date, setDate] = useState<Date | undefined>(new Date(assignedDate));
 
-    useEffect(() => {
-        if (setAssignedDate) {
-            setAssignedDate(date ? format(date, "yyyy-MM-dd") : "");
-        }
-    }, [date]);
+  useEffect(() => {
+    if (setAssignedDate) {
+      setAssignedDate(date ? format(date, "yyyy-MM-dd") : "");
+    }
+  }, [date]);
 
-    return (
-        <Popover>
-            <PopoverTrigger asChild>
-                <Button
-                    id={id}
-                    variant={"outline"}
-                    className={cn(
-                        "w-full justify-start text-left font-normal",
-                        !date && "text-muted-foreground",
-                    )}
-                >
-                    <CalendarIcon className="mr-2 h-4 w-4" />
-                    {date ? (
-                        format(date, "PPP", { locale: ru })
-                    ) : (
-                        <span>Выбрать дату</span>
-                    )}
-                </Button>
-            </PopoverTrigger>
-            <PopoverContent className="w-auto p-0">
-                <Calendar
-                    mode="single"
-                    locale={ru}
-                    selected={date}
-                    onSelect={setDate}
-                    initialFocus
-                />
-            </PopoverContent>
-        </Popover>
-    );
+  return (
+    <Popover>
+      <PopoverTrigger asChild>
+        <Button
+          id={id}
+          variant={"outline"}
+          className={cn(
+            "w-full justify-start text-left font-normal",
+            !date && "text-muted-foreground",
+          )}
+        >
+          <CalendarIcon className="mr-2 h-4 w-4" />
+          {date ? (
+            format(date, "PPP", { locale: ru })
+          ) : (
+            <span>Выбрать дату</span>
+          )}
+        </Button>
+      </PopoverTrigger>
+      <PopoverContent className="w-auto p-0">
+        <Calendar
+          mode="single"
+          locale={ru}
+          selected={date}
+          onSelect={setDate}
+          initialFocus
+        />
+      </PopoverContent>
+    </Popover>
+  );
 };
 
 export default DayPicker;
