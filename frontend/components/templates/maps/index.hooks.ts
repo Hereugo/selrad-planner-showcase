363da03724860568d45e469ba0fcacb2968dc7cb
nomomon <funisinfinity26@gmail.com>
2024-03-29 15:36:41 +0100

    Update HSV contrast colors

--- frontend/components/templates/maps/index.hooks.ts
+++ frontend/components/templates/maps/index.hooks.ts
@@ -4,14 +4,16 @@ import _ from "lodash";
 import { Polygon } from "@pbe/react-yandex-maps";
 
 // HSV contrast colors
-const colors = ['#1b9e77',
-    '#d95f02',
-    '#7570b3',
-    '#e7298a',
-    '#66a61e',
-    '#e6ab02',
-    '#a6761d',
-    '#666666'];
+const colors = ['#1f77b4',
+    '#ff7f0e',
+    '#2ca02c',
+    '#d62728',
+    '#9467bd',
+    '#8c564b',
+    '#e377c2',
+    '#7f7f7f',
+    '#bcbd22',
+    '#17becf'];
 
 
 export const useMaps = () => {
