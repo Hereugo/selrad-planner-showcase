7a67e23975a2de4126b72aa1920cfa147afb9274
nomomon <funisinfinity26@gmail.com>
2024-03-29 08:45:35 +0100

    Update top bar component styling

--- frontend/components/molecules/top-bar/index.tsx
+++ frontend/components/molecules/top-bar/index.tsx
@@ -4,21 +4,21 @@ import Link from "next/link";
 import { FC } from "react";
 
 interface TopBarProps {
-  className?: string;
+    className?: string;
 }
 
 const TopBar: FC<TopBarProps> = ({ className }) => {
-  return (
-    <div className={cn("border-b-2", className)}>
-      <Link
-        className="h-full text-2xl font-bold flex items-center px-6 gap-2"
-        href="/"
-      >
-        <CalendarDays />
-        <span>Планировщик</span>
-      </Link>
-    </div>
-  );
+    return (
+        <div className={cn("border-b-2", className)}>
+            <Link
+                className="w-64 h-full text-2xl font-bold flex items-center px-6 gap-2"
+                href="/"
+            >
+                <CalendarDays />
+                <span>Планировщик</span>
+            </Link>
+        </div>
+    );
 };
 
 export default TopBar;
