ba722b16150fcf3984bf2ce5a0b0fcea5e981df8
nomomon <funisinfinity26@gmail.com>
2024-04-01 02:06:13 +0200

    Update next.config.mjs and package.json

--- frontend/components/templates/maps/index.hooks.ts
+++ frontend/components/templates/maps/index.hooks.ts
@@ -1,11 +1,22 @@
 import hull from "hull.js";
 import { usePlans } from "../plans/index.hooks";
 import _ from "lodash";
-import { Polygon } from "@pbe/react-yandex-maps";
 import { useManagers } from "@/components/molecules/plan-dialog-new/index.hooks";
-
+import { useState } from "react";
 // HSV contrast colors
-const colors = [
+const pinColors = [
+  "#1f77b4",
+  "#ff7f0e",
+  "#2ca02c",
+  "#d62728",
+  "#9467bd",
+  "#8c564b",
+  "#e377c2",
+  "#7f7f7f",
+  "#bcbd22",
+  "#17becf",
+];
+const edgeColors = [
   "#1f77b4",
   "#ff7f0e",
   "#2ca02c",
@@ -22,17 +33,8 @@ export const useMaps = () => {
   const { plans } = usePlans();
   const { managers } = useManagers();
 
-  const mapCenter = plans.reduce(
-    (acc, plan) => [
-      acc[0] + Number(plan.client.address.lat) / plans.length,
-      acc[1] + Number(plan.client.address.lon) / plans.length,
-    ],
-    [0, 0],
-  );
-  if (plans.length === 0) {
-    mapCenter[0] = 43.238949;
-    mapCenter[1] = 76.889709;
-  }
+  const [selectedPlan, setSelectedPlan] = useState<Plan | undefined>();
+  const mapCenter = [43.238949, 76.889709];
 
   const plansByDay = _.groupBy(plans, "assigned_date");
 
@@ -43,50 +45,23 @@ export const useMaps = () => {
     let day = Object.keys(plansByDay)[i];
     let plans = plansByDay[day];
 
-    // Make placeMarks
     for (let plan of plans) {
-      placeMarks.push({
-        geometry: [
-          Number(plan.client.address.lat),
-          Number(plan.client.address.lon),
-        ],
-        properties: {
-          hintContent: plan.client.name,
-          balloonContent: [
-            `<b>${plan.assigned_date}</b>`,
-            plan.client.name,
-            plan.managers.map((m) => `<li>${m.first_name}</li>`).join("\n"),
-            plan.worklist.map((w) => `<ul>${w.name}</ul>`).join("\n"),
-            "<a href='#' class='text-blue-500'>Изменить</a>",
-          ].join("<br/>"),
-        },
-        options: {
-          iconColor: colors[i % colors.length],
-        },
-      });
+      placeMarks.push(
+        planToPlaceMark(plan, pinColors[i % pinColors.length], () => {
+          setSelectedPlan((p) => (p?.id !== plan.id ? plan : undefined));
+        }),
+      );
     }
 
-    // Make polygons
-    for (let manager of managers) {
-      const plansByManager = _.filter(plans, (plan, i) =>
-        plan.managers.map((m) => m.id).includes(manager.id),
+    for (let [idx, manager] of Object.entries(managers)) {
+      polygons.push(
+        ...managerPlansToPolygon(
+          plans,
+          manager,
+          edgeColors[Number(idx) % edgeColors.length],
+          (Number(idx) / managers.length) * 2e-3,
+        ),
       );
-      let managerCoordinates = plansByManager.map((plan) => [
-        Number(plan.client.address.lat),
-        Number(plan.client.address.lon),
-      ]);
-
-      if (managerCoordinates.length <= 1) continue;
-
-      let polygonCoords = hull(managerCoordinates, 50);
-      polygons.push({
-        geometry: [polygonCoords],
-        options: {
-          fillColor: colors[i % colors.length] + "33",
-          strokeColor: colors[i % colors.length],
-          strokeWidth: 2,
-        },
-      });
     }
   }
 
@@ -94,5 +69,62 @@ export const useMaps = () => {
     mapCenter,
     placeMarks,
     polygons,
+    selectedPlan,
+    setSelectedPlan,
   };
+};
+
+const planToPlaceMark = (plan: Plan, color: string, callback: Function) => {
+  return {
+    geometry: [
+      Number(plan.client.address.lat),
+      Number(plan.client.address.lon),
+    ],
+    properties: {
+      // hintContent: plan.client.name,
+      // balloonContent: planToBalloonHTML(plan),
+    },
+    onClick: callback,
+    options: {
+      iconColor: color,
+    },
+  };
+};
+
+const planToBalloonHTML = (plan: Plan) => {
+  return `
+        <div>
+            <h3 class="text-lg font-semibold">${plan.client.name}</h3>
+            <p>Адрес: ${plan.client.address.street}</p>
+        </div>
+    `;
+};
+
+const managerPlansToPolygon = (
+  dayPlans: Plan[],
+  manager: Manager,
+  color: string,
+  displace = 0,
+) => {
+  const managerPlans = _.filter(dayPlans, (plan, _) =>
+    plan.managers.map((m) => m.id).includes(manager.id),
+  );
+  const managerCoordinates = managerPlans.map((plan) => [
+    Number(plan.client.address.lat) + displace,
+    Number(plan.client.address.lon) + displace,
+  ]);
+
+  if (managerCoordinates.length <= 1) return [];
+
+  const polygonCoords = hull(managerCoordinates, 50);
+  return [
+    {
+      geometry: [polygonCoords],
+      options: {
+        fillColor: color + "33",
+        strokeColor: color,
+        strokeWidth: 2,
+      },
+    },
+  ];
 };
--- frontend/components/templates/maps/index.tsx
+++ frontend/components/templates/maps/index.tsx
@@ -1,48 +1,136 @@
 "use client";
 
 import { FC } from "react";
-import {
-  Polygon,
-  Placemark,
-  YMaps,
-  Map,
-  Polyline,
-} from "@pbe/react-yandex-maps";
+import { Polygon, Placemark, YMaps, Map } from "@pbe/react-yandex-maps";
 import { useMaps } from "./index.hooks";
+import PlanDialogEdit from "@/components/molecules/plan-dialog-edit";
+import { Button } from "@/components/ui/button";
+import {
+  CircleXIcon,
+  Earth,
+  PackageOpenIcon,
+  Pen,
+  PenBoxIcon,
+  PenIcon,
+  XIcon,
+} from "lucide-react";
+import { cn, managerFullName } from "@/lib/utils";
+import { TengeReciept } from "@/components/icons/tenge-reciept";
 
 interface MapsTemplateProps {}
 
 const MapsTemplate: FC<MapsTemplateProps> = () => {
-  const { mapCenter, placeMarks, polygons } = useMaps();
+  const { mapCenter, placeMarks, polygons, selectedPlan, setSelectedPlan } =
+    useMaps();
 
   return (
-    <YMaps>
-      <Map
-        defaultState={{
-          center: mapCenter,
-          zoom: 12,
-        }}
-        className="w-full h-full"
+    <div className="flex h-full gap-4">
+      <YMaps>
+        <Map
+          state={{
+            center: mapCenter,
+            zoom: 12,
+          }}
+          className="flex-[2] h-full rounded-lg overflow-clip duration-300"
+        >
+          {placeMarks.map((placeMark, index) => (
+            <Placemark
+              key={index}
+              {...placeMark}
+              modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
+            />
+          ))}
+          {polygons.map((polygon, index) => (
+            <Polygon
+              key={index}
+              geometry={polygon.geometry}
+              options={polygon.options}
+              modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
+            />
+          ))}
+        </Map>
+      </YMaps>
+      <div
+        className={cn(
+          "h-full overflow-y-auto duration-300",
+          `flex-[${Number(!!selectedPlan)}]`,
+        )}
       >
-        {placeMarks.map((placeMark, index) => (
-          <Placemark
-            key={index}
-            geometry={placeMark.geometry}
-            properties={placeMark.properties}
-            options={placeMark.options}
-            modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
-          />
-        ))}
-        {polygons.map((polygon, index) => (
-          <Polyline
-            key={index}
-            geometry={polygon.geometry}
-            options={polygon.options}
-            modules={["geoObject.addon.balloon", "geoObject.addon.hint"]}
-          />
-        ))}
-      </Map>
-    </YMaps>
+        {selectedPlan && (
+          <div className="flex flex-col gap-4 relative pt-8">
+            <CircleXIcon
+              className="fill-red-400 stroke-red-700 w-6 h-6 cursor-pointer absolute top-2 right-0"
+              onClick={() => setSelectedPlan(undefined)}
+            />
+            <div>
+              <div
+                className="text-lg font-bold line-clamp-1 text-ellipsis"
+                title={selectedPlan.client.name}
+              >
+                {selectedPlan.client.name}
+              </div>
+              <div className="text-sm text-muted-foreground">
+                {selectedPlan.client.address.street}
+              </div>
+            </div>
+
+            <div>
+              <div className="font-semibold">Менеджеры:</div>
+              <div className="grid grid-cols-3">
+                {selectedPlan.managers
+                  .sort((a, b) => a.first_name.localeCompare(b.first_name))
+                  .map((manager) => (
+                    <div key={manager.id}>{managerFullName(manager)}</div>
+                  ))}
+              </div>
+            </div>
+
+            <div>
+              <div className="font-semibold">Работы:</div>
+              <div className="grid grid-cols-3">
+                {selectedPlan.worklist
+                  .sort((a, b) => a.name.localeCompare(b.name))
+                  .map((work) => (
+                    <div key={work.id}>{work.name}</div>
+                  ))}
+              </div>
+            </div>
+
+            <div>
+              <div className="font-semibold">Детали:</div>
+              <div className="grid grid-cols-2 items-center">
+                <span className="flex items-center">
+                  <TengeReciept className="w-5 h-5 mr-2" />
+                  Сумма отгрузки
+                </span>
+                <span>{selectedPlan.shipment_cost}</span>
+              </div>
+              <div className="grid grid-cols-2">
+                <span className="flex items-center">
+                  <PackageOpenIcon className="w-5 h-5 mr-2" />
+                  Кол. коробок
+                </span>
+                <span>{selectedPlan.box_count}</span>
+              </div>
+            </div>
+
+            <div className="w-full gap-2 flex flex-row">
+              <PlanDialogEdit plan={selectedPlan}>
+                <Button className="flex-1">
+                  <PenBoxIcon className="w-5 h-5 mr-2" />
+                  Изменить
+                </Button>
+              </PlanDialogEdit>
+
+              <Button className="flex-1">
+                <Earth className="w-5 h-5 mr-2" />
+                Клиенты рядом
+              </Button>
+            </div>
+          </div>
+        )}
+      </div>
+    </div>
   );
 };
 
