e0e1d4882d7ed9c0bd3725dd3175d63bf4e05f9e
nomomon <funisinfinity26@gmail.com>
2024-04-01 02:30:02 +0200

    Refactor plans API calls

--- frontend/lib/backend/plans/index.ts
+++ frontend/lib/backend/plans/index.ts
@@ -1,116 +1,114 @@
 import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
 import urls from "../urls";
 import {
-  deleteWithAuth,
-  fetchWithAuth,
-  patchWithAuth,
-  postWithAuth,
+    deleteWithAuth,
+    fetchWithAuth,
+    patchWithAuth,
+    postWithAuth,
 } from "../httpCalls";
 
 interface plansQueryProps {
-  date_after?: string;
-  date_before?: string;
-  limit?: number;
-  ordering?: string;
-  page?: number;
-  search?: string;
+    date_after?: string;
+    date_before?: string;
+    limit?: number;
+    ordering?: string;
+    page?: number;
+    search?: string;
 }
 
 export const usePlansQuery = (props: plansQueryProps = {}) => {
-  const url = urls.base_backend.plans;
-  const queryParams = [];
+    const url = urls.base_backend.plans;
+    const queryParams = [];
 
-  if (props.date_after) queryParams.push(`date_after=${props.date_after}`);
-  if (props.date_before) queryParams.push(`date_before=${props.date_before}`);
-  if (props.limit) queryParams.push(`limit=${props.limit}`);
-  if (props.ordering) queryParams.push(`ordering=${props.ordering}`);
-  if (props.page) queryParams.push(`page=${props.page}`);
-  if (props.search) queryParams.push(`search=${props.search}`);
+    if (props.date_after) queryParams.push(`date_after=${props.date_after}`);
+    if (props.date_before) queryParams.push(`date_before=${props.date_before}`);
+    if (props.limit) queryParams.push(`limit=${props.limit}`);
+    if (props.ordering) queryParams.push(`ordering=${props.ordering}`);
+    if (props.page) queryParams.push(`page=${props.page}`);
+    if (props.search) queryParams.push(`search=${props.search}`);
 
-  const urlWithParams = `${url}${queryParams.length > 0 ? "?" : ""}${queryParams.join("&")}`;
+    const urlWithParams = `${url}${queryParams.length > 0 ? "?" : ""}${queryParams.join("&")}`;
 
-  return useQuery(["usePlansQuery", urlWithParams], async () =>
-    fetchWithAuth<Plan[]>(urlWithParams),
-  );
+    return useQuery(["usePlansQuery", urlWithParams], async () =>
+        fetchWithAuth<Plan[]>(urlWithParams),
+    );
 };
 
 export const usePlanQuery = (id: string) => {
-  const url = urls.base_backend.plans;
-  const urlParamed = `${url}/${id}/`;
+    const url = urls.base_backend.plans;
+    const urlParamed = `${url}/${id}/`;
 
-  return useQuery(["usePlanQuery", id], async () =>
-    fetchWithAuth<Plan>(urlParamed),
-  );
+    return useQuery(["usePlanQuery", id], async () =>
+        fetchWithAuth<Plan>(urlParamed),
+    );
 };
 
 interface planCreateMutationProps {
-  assigned_date: string;
-  client: string;
-  managers: number[];
-  worklist: string[];
-  shipment_cost: string;
-  box_count: string;
-  comment: string;
+    assigned_date: string;
+    client: string;
+    managers: number[];
+    worklist: string[];
+    shipment_cost: string;
+    box_count: string;
+    comment: string;
 }
 
 export const usePlanCreateMutation = () => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  const url = urls.base_backend.plans + "/";
+    const url = urls.base_backend.plans + "/";
 
-  const call = (plan: planCreateMutationProps) => {
-    return postWithAuth(url, plan);
-  };
+    const call = (plan: planCreateMutationProps) => {
+        return postWithAuth(url, plan);
+    };
 
-  return useMutation(call, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["usePlansQuery"]);
-    },
-  });
+    return useMutation(call, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(["usePlansQuery"]);
+        },
+    });
 };
 
 interface planUpdateMutation {
-  assigned_date: string;
-  client: string;
-  managers: number[];
-  worklist: string[];
-  shipment_cost: string;
-  box_count: string;
-  comment: string;
+    assigned_date: string;
+    client: string;
+    managers: number[];
+    worklist: string[];
+    shipment_cost: string;
+    box_count: string;
+    comment: string;
 }
 
 export const usePlanUpdateMutation = (id: string) => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  const url = urls.base_backend.plans;
-  const urlParamed = `${url}/${id}/`;
+    const url = urls.base_backend.plans;
+    const urlParamed = `${url}/${id}/`;
 
-  const call = (plan: planUpdateMutation) => {
-    return patchWithAuth(urlParamed, plan);
-  };
+    const call = (plan: planUpdateMutation) => {
+        return patchWithAuth(urlParamed, plan);
+    };
 
-  return useMutation(call, {
-    onSuccess: () => {
-      queryClient.invalidateQueries({
-        predicate: (query) => query.queryKey[0] === "usePlansQuery",
-      });
-    },
-  });
+    return useMutation(call, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(["usePlansQuery"]);
+        },
+    });
 };
 
 export const useDeletePlanMutation = (id: string) => {
-  const queryClient = useQueryClient();
+    const queryClient = useQueryClient();
 
-  const url = urls.base_backend.plans;
-  const urlParamed = `${url}/${id}/`;
+    const url = urls.base_backend.plans;
+    const urlParamed = `${url}/${id}/`;
 
-  const call = () => {
-    return deleteWithAuth(urlParamed);
-  };
+    const call = () => {
+        return deleteWithAuth(urlParamed);
+    };
 
-  return useMutation(call, {
-    onSuccess: () => {
-      queryClient.invalidateQueries(["usePlansQuery"]);
-    },
-  });
+    return useMutation(call, {
+        onSuccess: () => {
+            queryClient.invalidateQueries(["usePlansQuery"]);
+        },
+    });
 };
