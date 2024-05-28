4b509c5dbc80ab268b41b170b418ee8ebda935ab
nomomon <funisinfinity26@gmail.com>
2024-05-28 07:10:12 +0200

    chore: change icon

--- frontend/components/templates/maps/max-days-alert.tsx
+++ frontend/components/templates/maps/max-days-alert.tsx
@@ -1,11 +1,11 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
-import { Terminal } from "lucide-react";
+import { TriangleAlert } from "lucide-react";
 
 const MaxDaysAlert = () => {
   return (
     <div className="absolute top-[50%] right-[50%] translate-x-[50%] -translate-y-[50%] z-50 w-96">
       <Alert variant={"warning"}>
-        <Terminal className="h-4 w-4" />
+        <TriangleAlert className="h-4 w-4" />
         <AlertTitle>Внимание!</AlertTitle>
         <AlertDescription>
           Вы выбрали слишком длинный период дат. Пожалуйста, выберите период из
