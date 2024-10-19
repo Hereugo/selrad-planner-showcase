5c3187165b29a434c6740ba2b235fd75a8cc6233
Амир Нурмухамбетов <thereug6@gmail.com>
2024-10-19 19:00:12 +0200

    working version

--- frontend/lib/backend/urls.ts
+++ frontend/lib/backend/urls.ts
@@ -11,11 +11,11 @@ const urls = {
   base_backend: {
     plans: `${BASE_BACKEND_V1}/plans`,
     clients: `${BASE_BACKEND_V1}/clients`,
-    compare: `${BASE_BACKEND_V1}/clients/export_compare_years`,
     work_items: `${BASE_BACKEND_V1}/work_items`,
     plan_export: `${BASE_BACKEND_V1}/plans/export`,
     dispatch_export_report: `${BASE_BACKEND_V1}/plans/dispatch_report`,
     plan_export_report: `${BASE_BACKEND_V1}/plans/export_report/`,
+    compare: `${BASE_BACKEND_V1}/plans/export_compare_years`,
     managers: `${BASE_BACKEND_V1}/users/managers`,
     warehousers: `${BASE_BACKEND_V1}/users/warehousers`,
     drivers: `${BASE_BACKEND_V1}/users/drivers`,
