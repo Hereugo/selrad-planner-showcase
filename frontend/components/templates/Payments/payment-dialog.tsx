3bd3dbd5237cdae3bc8cb02c85b3e39c9df79fa3
nomomon <funisinfinity26@gmail.com>
2024-12-19 19:40:47 +0100

    update dialog

--- frontend/components/templates/Payments/payment-dialog.tsx
+++ frontend/components/templates/Payments/payment-dialog.tsx
@@ -1,5 +1,14 @@
-import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
+import {
+  Dialog,
+  DialogContent,
+  DialogFooter,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "@/components/ui/dialog";
 import { FC, ReactNode } from "react";
+import { useUpdatePaymentRegistry } from "./index.hooks";
+import { Button } from "@/components/ui/button";
 
 interface PaymentDialogProps {
   paymentRegistry: PaymentRegistry;
@@ -10,10 +19,20 @@ const PaymentDialog: FC<PaymentDialogProps> = ({
   paymentRegistry,
   children,
 }) => {
+  const {} = useUpdatePaymentRegistry(paymentRegistry);
+
   return (
     <Dialog>
       <DialogTrigger asChild>{children}</DialogTrigger>
-      <DialogContent className="max-w-md">hi</DialogContent>
+      <DialogContent className="max-w-xl">
+        <DialogHeader>
+          <DialogTitle className="mb-4">Изменить выплату</DialogTitle>
+        </DialogHeader>
+        <div className="flex gap-4 flex-col justify-stretch w-full"></div>
+        <DialogFooter>
+          <Button>Подтвердить выплату</Button>
+        </DialogFooter>
+      </DialogContent>
     </Dialog>
   );
 };
