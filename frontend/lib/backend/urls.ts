bd7d59b63167f7ac2b31afa6537e385e42364fd3
nomomon <funisinfinity26@gmail.com>
2025-01-05 21:11:05 +0500

    refactor: update report URL to point to manager_report

--- frontend/lib/backend/urls.ts
+++ frontend/lib/backend/urls.ts
@@ -26,7 +26,7 @@ const urls = {
       dispatch_report: `${BASE_BACKEND_V1}/exports/dispatch_report`,
       payment_report: `${BASE_BACKEND_V1}/exports/payment_report`,
       plans: `${BASE_BACKEND_V1}/exports/plans`,
-      report: `${BASE_BACKEND_V1}/exports/report`,
+      report: `${BASE_BACKEND_V1}/exports/manager_report`,
     },
   },
 };
